22f32f19d711073c023b52dab1fd4ddc38c0e075
Sean Paul Harsevoort <seanpaulharsevoort@MacBook-Pro-Van-Sean-Paul.local>
2020-10-05 21:17:34 +0200

    Updates

---
+++ precache-manifest.ee0cd53b8ed6884e9657e97ce68f73c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f56e5acd62b2b6014d084bda86382c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "566cb49ad2a752527beb",
+    "url": "/monsters-rolodex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f01c86c8fbcdaaf17bcd",
+    "url": "/monsters-rolodex/static/js/2.8d4740e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-rolodex/static/js/2.8d4740e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566cb49ad2a752527beb",
+    "url": "/monsters-rolodex/static/js/main.00d7f6d7.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
